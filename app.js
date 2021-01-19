449c295fa66b9be5e440be828a616a562ed4a601
Lee Stratman <stratmanlc10@gmail.com>
2021-01-18 20:49:47 -0600

    Add user input to update or confirm the day trip.

--- app.js
+++ app.js
@@ -24,24 +24,77 @@ const ENTERTAINMENT = [
   "Shopping",
 ];
 
+let dayTrip = generateDayTrip();
+let confirmed = false;
+
+while (!confirmed) {
+  displayDayTrip(dayTrip);
+  let command = prompt(
+    "Your day trip is below. To refresh, enter 'a'.  To update an option, enter the first letter.  To confirm your trip, enter 'confirm'"
+  );
+
+  switch (command) {
+    case "a":
+      dayTrip = generateDayTrip();
+      break;
+    case "d":
+      dayTrip = updateDayTrip(dayTrip, "destination");
+      break;
+    case "r":
+      dayTrip = updateDayTrip(dayTrip, "restaurant");
+      break;
+    case "t":
+      dayTrip = updateDayTrip(dayTrip, "transportation");
+      break;
+    case "e":
+      dayTrip = updateDayTrip(dayTrip, "entertainment");
+      break;
+    case "confirm":
+      confirmed = true;
+      console.log("confirmed");
+      break;
+    default:
+      console.log("Invalid command, please try again!");
+  }
+}
+
 function generateDayTrip() {
-  let dayTrip = {
-    destination: "",
-    restaurant: "",
-    transportation: "",
-    entertainment: "",
+  return {
+    destination: DESTINATIONS[randomNumber(DESTINATIONS.length)],
+    restaurant: RESTAURANTS[randomNumber(RESTAURANTS.length)],
+    transportation: TRANSPORTATION[randomNumber(TRANSPORTATION.length)],
+    entertainment: ENTERTAINMENT[randomNumber(ENTERTAINMENT.length)],
   };
+}
 
-  dayTrip.destination = DESTINATIONS[randomNumber(DESTINATIONS.length)];
-  dayTrip.restaurant = RESTAURANTS[randomNumber(RESTAURANTS.length)];
-  dayTrip.transportation = TRANSPORTATION[randomNumber(TRANSPORTATION.length)];
-  dayTrip.entertainment = ENTERTAINMENT[randomNumber(ENTERTAINMENT.length)];
+function updateDayTrip(trip, tripProperty) {
+  switch (tripProperty) {
+    case "destination":
+      trip.destination = DESTINATIONS[randomNumber(DESTINATIONS.length)];
+      break;
+    case "restaurant":
+      trip.restaurant = RESTAURANTS[randomNumber(RESTAURANTS.length)];
+      break;
+    case "transportation":
+      trip.transportation = TRANSPORTATION[randomNumber(TRANSPORTATION.length)];
+      break;
+    case "entertainment":
+      trip.entertainment = ENTERTAINMENT[randomNumber(ENTERTAINMENT.length)];
+      break;
+  }
 
-  console.log(dayTrip);
+  return trip;
+}
+
+function displayDayTrip(dayTrip) {
+  console.clear();
+  console.log("Your Day Trip: \n");
+  console.log("Destination: ", dayTrip.destination);
+  console.log("Restaurant: ", dayTrip.restaurant);
+  console.log("Transportation: ", dayTrip.transportation);
+  console.log("Entertainment: ", dayTrip.entertainment);
 }
 
 function randomNumber(max) {
   return Math.floor(Math.random() * max);
 }
-
-generateDayTrip();
