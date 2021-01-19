b7c2845b70c88c4ce698a817ffb4a388693eb6e8
Lee Stratman <stratmanlc10@gmail.com>
2021-01-18 19:55:41 -0600

    Add options for destinations, restaurants, transportation, & entertainment

---
+++ app.js
@@ -0,0 +1,25 @@
+"use strict";
+
+const DESTINATIONS = [
+  "Wisconsin Dells",
+  "Chippewa Falls",
+  "Spring Green",
+  "Baraboo",
+  "Door County",
+];
+const RESTAURANTS = [
+  "Pizza Hut",
+  "Taco John's",
+  "Applebee's",
+  "Culvers",
+  "Chick-fil-A",
+];
+const TRANSPORTATION = ["Bus", "Train", "Bike", "Car"];
+const ENTERTAINMENT = [
+  "Museum",
+  "Zoo",
+  "Park",
+  "Theater",
+  "Brewery Tour",
+  "Shopping",
+];
